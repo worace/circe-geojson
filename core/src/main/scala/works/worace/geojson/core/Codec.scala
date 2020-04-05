b85ccd511bdc17b9e0d66b321f74d7d9c73b8809
Horace Williams <horace.d.williams@gmail.com>
2020-04-05 14:26:55 -0700

    Rename foreign member file to provide base Codec trait

--- core/src/main/scala/works/worace/geojson/core/Codec.scala
+++ core/src/main/scala/works/worace/geojson/core/Codec.scala
@@ -1,6 +1,7 @@
 package works.worace.geojson.core
 
-import io.circe.{JsonObject, Decoder, Encoder}
+import io.circe._
+import io.circe.Decoder.Result
 import io.circe.syntax._
 
 trait Codec[T <: GeoJson] {
@@ -9,8 +10,28 @@ trait Codec[T <: GeoJson] {
     withBBox(withForeignMembers(asJsonObject(gj), gj.foreignMembers), gj.bbox).asJson
   }
   val decoder: Decoder[T]
+
+  def decodeWithForeignMembers[T <: GeoJson](cursor: HCursor, baseDecoder: Decoder[T], handler: (T, Option[JsonObject]) => T): Result[T] = {
+    cursor.as[JsonObject].flatMap { obj =>
+      Json.fromJsonObject(obj)
+        .as[T](baseDecoder)
+        .map { base =>
+          handler(base, foreignMembers(obj))
+        }
+    }
+  }
+
   val coreKeys =
     Set("type", "geometry", "coordinates", "properties", "features", "geometries", "id", "bbox")
+
+  def foreignMembers(json: JsonObject): Option[JsonObject] = {
+    val foreignMembers = json.filterKeys(!coreKeys.contains(_))
+    if (foreignMembers.nonEmpty) {
+      Some(foreignMembers)
+    } else {
+      None
+    }
+  }
 
   def withForeignMembers(encoded: JsonObject, fm: Option[JsonObject]): JsonObject = {
     fm.map { obj =>
