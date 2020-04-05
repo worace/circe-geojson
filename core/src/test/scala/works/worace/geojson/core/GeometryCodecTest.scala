23cb2039c4eba0217d4482f2444b4fda8452e95f
Horace Williams <horace.d.williams@gmail.com>
2020-04-05 14:26:03 -0700

    Move codec case helper into TestHelpers

--- core/src/test/scala/works/worace/geojson/core/GeometryCodecTest.scala
+++ core/src/test/scala/works/worace/geojson/core/GeometryCodecTest.scala
@@ -4,20 +4,9 @@ import io.circe.{Json, JsonObject, Decoder, DecodingFailure}
 import io.circe.parser.decode
 import scala.reflect.ClassTag
 
-class GeometryCodecTest extends munit.FunSuite {
+class GeometryCodecTest extends munit.FunSuite with TestHelpers {
   import TestData._
   import GeometryCodec.implicits._
-
-  def codecCase[G <: GeoJson: ClassTag](c: Case)(implicit decoder: Decoder[G]) = {
-    c.decoded match {
-      case g: G => {
-        decode[G](c.encoded)
-          .map((res: G) => assert(res == g))
-          .getOrElse(fail("Parsing failure"))
-      }
-      case _ => fail("GeometryCases should only be run against Geometries")
-    }
-  }
 
   test("round-tripping base cases") {
     BaseGeomCases.all.foreach { c => codecCase[Geometry](c) }
--- core/src/test/scala/works/worace/geojson/core/TestHelpers.scala
+++ core/src/test/scala/works/worace/geojson/core/TestHelpers.scala
@@ -1,14 +1,32 @@
 package works.worace.geojson.core
 
+import scala.reflect.ClassTag
+import io.circe.parser.decode
+import io.circe.{Json, JsonObject, Decoder, DecodingFailure}
+
 trait TestHelpers extends munit.FunSuite {
+  import TestData.Case
   import io.circe.syntax._
   import GeoJsonSerde._
   def roundTripCase(gj: GeoJson) {
     val encoded = gj.asJson
     GeoJson
       .fromJson(encoded)
-      .map { decoded => assertEquals(decoded, gj) }
-      .toOption
-      .getOrElse(fail(s"Failed round-trip: ${gj}"))
+      .fold(
+        err => fail(s"Failed round-trip: ${gj}", clues(gj, err, encoded)),
+        decoded => assertEquals(decoded, gj)
+      )
+
+  }
+
+  def codecCase[G <: GeoJson: ClassTag](c: Case)(implicit decoder: Decoder[G]) = {
+    c.decoded match {
+      case g: G => {
+        decode[G](c.encoded)
+          .map((res: G) => assertEquals(res, g))
+          .getOrElse(fail("Parsing failure"))
+      }
+      case _ => fail("GeometryCases should only be run against Geometries")
+    }
   }
 }
